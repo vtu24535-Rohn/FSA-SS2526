d44ddda643b996cac58ba6fcb8502d8e8cb09b49
vtu24535-Rohn <vtu24535@veltech.edu.in>
2026-03-16 11:47:10 +0530

    Add files via upload

---
+++ Task 8/spring-employee-core/src/main/java/com/example/emp/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.emp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.emp")
+public class AppConfig {
+}
